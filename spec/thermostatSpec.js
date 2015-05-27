8d66257b86ca959833ef3e53c9d7278a4ff66ee9
Anna <anna.klimas@gmail.com>
2015-05-27 14:16:42 +0100

    all engine tests passing, model and functions complete

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,9 +1,105 @@
 describe("Thermostat", function(){
   var thermy;
+
+  beforeEach(function() {
+    thermy = new Thermostat();
+  });
+
   describe('temperature is', function(){
     it('20 by default', function(){
-      thermy = new Thermostat();
       expect(thermy.currentTemperature()).toEqual(20);
+    });
+    it('increased by one when the up botton is pressed', function(){
+      thermy.upButton();
+      expect(thermy.currentTemperature()).toEqual(21);
+    });
+    it('decreased by one when the down button is pressed', function(){
+      thermy.downButton();
+      expect(thermy.currentTemperature()).toEqual(19);
+    });
+    it('reset to 20 when the reset button is pressed', function(){
+      thermy.downButton();
+      thermy.resetButton();
+      expect(thermy.currentTemperature()).toEqual(20);
+    });
+  });
+
+  describe('Power Saving mode is', function(){
+    it('on by default', function(){
+      expect(thermy.powerSaving()).toEqual(true);
+    });
+    it('off when switched off', function(){
+      thermy.powerSavingOff();
+      expect(thermy.powerSaving()).toEqual(false);
+    });
+    it('on when switched back on', function(){
+      thermy.powerSavingOff();
+      thermy.powerSavingOn();
+      expect(thermy.powerSaving()).toEqual(true);
+    });
+  });
+
+  describe('minimum temperature', function(){
+    it('is 10', function(){
+      thermy.downButton();
+      thermy.downButton();
+      thermy.downButton();
+      thermy.downButton();
+      thermy.downButton();
+      thermy.downButton();
+      thermy.downButton();
+      thermy.downButton();
+      thermy.downButton();
+      thermy.downButton();
+      thermy.downButton();
+      expect(thermy.currentTemperature()).toEqual(10);
+    });
+  });
+
+  describe('maximum temperature', function(){
+    it('is 25 when Power Saving mode is on', function(){
+     thermy.upButton();
+     thermy.upButton();
+     thermy.upButton();
+     thermy.upButton();
+     thermy.upButton();
+     thermy.upButton();
+     expect(thermy.currentTemperature()).toEqual(25);
+    });
+    it('is 32 when Power Saving mode is off', function(){
+      thermy.powerSavingOff();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      expect(thermy.currentTemperature()).toEqual(32);
+    });
+    it('is pushed back down to 25 if above that when Power Saving turns on', function(){
+      thermy.powerSavingOff();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.upButton();
+      thermy.powerSavingOn();
+      expect(thermy.currentTemperature()).toEqual(25);
     });
 
   });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,41 @@
 function Thermostat(){
 this.temp = 20;
+this.powerSavingMode = true;
+this.minTemperature = 10;
+// this.maxTemperature = 25;
 };
 
 Thermostat.prototype.currentTemperature = function() {
   return this.temp;
+};
+Thermostat.prototype.upButton = function() {
+  if (this.temp < this.maxTemperature()) {
+    this.temp = this.temp + 1;
+  }
+};
+Thermostat.prototype.downButton = function() {
+  if (this.temp > this.minTemperature) {
+    this.temp = this.temp - 1;
+  }
+};
+Thermostat.prototype.resetButton = function() {
+  this.temp = 20;
+};
+Thermostat.prototype.powerSaving = function() {
+  return this.powerSavingMode;
+};
+Thermostat.prototype.powerSavingOn = function() {
+  this.powerSavingMode = true;
+  this.temp = Math.min(this.temp, this.maxTemperature());
+};
+Thermostat.prototype.powerSavingOff = function() {
+  this.powerSavingMode = false;
+};
+Thermostat.prototype.maxTemperature = function() {
+  if (this.powerSavingMode) {
+    return 25;
+  }
+  else {
+    return 32;
+  };
 };
