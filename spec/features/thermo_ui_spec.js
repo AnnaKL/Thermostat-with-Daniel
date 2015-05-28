5518b3f64f5238217f60c094b34a2eac246234e7
DanielJohnston <macademy@danieljohnston.co.uk>
2015-05-28 11:35:02 +0100

    added more UI tests, all passing

--- spec/features/thermo_ui_spec.js
+++ spec/features/thermo_ui_spec.js
@@ -1,4 +1,4 @@
-describe('Thermostat', function(){
+describe('Thermostat UI', function(){
 
   beforeEach(function(){
     jasmine.getFixtures().fixturesPath = '.';
@@ -8,6 +8,37 @@ describe('Thermostat', function(){
 
   it('displays default temperature', function(){
     expect('#temp').toContainText('20');
+  });
+
+  it('increases temperature when Up button clicked', function(){
+    $('#up').click();
+    expect('#temp').toContainText('21');
+  });
+
+  it('decreases temperature when Down button clicked', function(){
+    $('#down').click();
+    expect('#temp').toContainText('19');
+  });
+
+  it('reset temperature to 20 when reset button clicked', function(){
+    $('#down').click();
+    $('#reset').click();
+    expect('#temp').toContainText('20');
+  });
+
+  it('Power Saving Mode is on when open the page', function(){
+    expect('#power-saving').toContainText('true');
+  });
+
+  it('Power Saving Mode is off after turning off', function(){
+    $('#powersaveoff').click();
+    expect('#power-saving').toContainText('false');
+  });
+
+  it('Power Saving Mode is on after turning off and on', function(){
+    $('#powersaveoff').click();
+    $('#powersaveon').click();
+    expect('#power-saving').toContainText('true');
   });
 
 });
