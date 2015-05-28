0beb77439a98cb3615f13a5410bba8edc9f9290d
DanielJohnston <macademy@danieljohnston.co.uk>
2015-05-28 17:36:25 +0100

    api and server working fine, tests broken by folder structure changes

--- public/src/Helpers.js
+++ public/src/Helpers.js
@@ -17,11 +17,11 @@ Thermostat.prototype.PowerOn = function() {
   this.powerSavingOn();
   document.getElementById('power-saving').innerHTML = thermos.powerSavingMode;
   document.getElementById('temp').innerHTML = thermos.temp;
-  // post powersavingmode to server
-  // post temperature to server
+  $.post('http://localhost:9292/powerSavingMode', { powersavingmode: true });
+  $.post('http://localhost:9292/temp', { temp: thermos.temp });
 };
 Thermostat.prototype.PowerOff = function() {
   this.powerSavingOff();
   document.getElementById('power-saving').innerHTML = thermos.powerSavingMode;
-  // post powersavingmode to server
+  $.post('http://localhost:9292/powerSavingMode', { powersavingmode: false });
 };
